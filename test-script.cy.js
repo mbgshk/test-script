d0d53266b21a3474eaa9de45c3ab4b6a28427ac4
mbgshk <mbgshk@gmail.com>
2022-07-06 20:10:00 +0700

    Make an assertion

--- test-script.cy.js
+++ test-script.cy.js
@@ -1,9 +1,12 @@
 /// <reference types="cypress" />
 
 describe('My First Test', () =>{
-    it('Visits the Kitchen Sink', () => {
+    it('clicking "type" navigates to a new url', () => {
         cy.visit('https://example.cypress.io')
 
         cy.contains('type').click()
+
+        // Should be on a new URL which includes '/commands/actions/'
+        cy.url().should('include', '/commands/actions/')
     })
 })
